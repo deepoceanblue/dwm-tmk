bcaae8f1a00cdd0cff4af833fd9b4b81ba8a1ac7
deepoceanblue <tomek@roslinka.localdomain>
2020-01-29 23:36:19 +0100

    added keybindings for {view,tag}to{left,right}

--- config.h
+++ config.h
@@ -96,8 +96,12 @@ static Key keys[] = {
 	/* { MODKEY,                       XK_period, focusmon,       {.i = +1 } }, */
 	/* { MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } }, */
 	/* { MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } }, */
-	{ MODKEY|ShiftMask,             XK_n, focusmon,     {.i = +1 } },
-	{ MODKEY|ControlMask|ShiftMask, XK_n, tagmon,       {.i = +1 } },
+	{ MODKEY,             XK_n, viewtoright,     {0} },
+	{ MODKEY|ShiftMask, XK_n, viewtoleft,       {0} },
+	{ MODKEY,             XK_t, tagtoright,     {0} },
+	{ MODKEY|ShiftMask, XK_t, tagtoleft,       {0} },
+	/* { MODKEY|ShiftMask,             XK_n, focusmon,     {.i = +1 } }, */
+	/* { MODKEY|ControlMask|ShiftMask, XK_n, tagmon,       {.i = +1 } }, */
 	TAGKEYS(                        XK_1,                      0)
 	TAGKEYS(                        XK_2,                      1)
 	TAGKEYS(                        XK_3,                      2)
